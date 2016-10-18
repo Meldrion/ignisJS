183514893c582703567ca26d2de6549a5b0ba053
Fabien Steines <fabien.steines@gmail.com>
2016-10-18 11:47:53 +0200

    update

---
+++ js/MapTree.js
@@ -0,0 +1,19 @@
+function MapTree() {
+
+}
+
+MapTree.prototype.getTree = function() {
+
+    return [{
+        text: "Project",
+        nodes: [
+            {
+                text: "Map 0001"
+            },
+            {
+                text: "Map 0002"
+            }
+        ]
+    }]
+
+};
